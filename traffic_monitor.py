40436acb6b006991730bf20c3c232a70359addfa
Alex Burrows <burrows.alexj@gmail.com>
2025-01-02 08:49:12 +0000

    Only log traffic going to the server

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -4,10 +4,21 @@ from datetime import datetime
 import os
 import json
 from collections import deque
+import netifaces
+import socket
+
+def get_ip():
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            s.connect(('8.8.8.8', 80))
+            return s.getsockname()[0]
+    except Exception as e:
+        return f'Error: {e}'
 
 CONN_ATTEMPT_THRESHOLD = 2
 LOG_FILE = 'traffic.log'
 CONN_ATTEMPTS_FILE = "connection_attempts.json"
+IP=get_ip()
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(message)s')
 
 def get_timestamps():
@@ -15,6 +26,8 @@ def get_timestamps():
     timestamp_str = timestamp_val.strftime('%Y-%m-%d %H-:%M:%S')
 
     return timestamp_str, timestamp_val
+
+
 
 
 def log_packet(packet):
@@ -25,6 +38,9 @@ def log_packet(packet):
         dst_ip = packet[0][1].dst
         src_port = packet[0][2].sport
         dst_port = packet[0][2].dport
+
+        if dst_ip != IP:
+            return
 
         log_entry = f'[{timestamp_str}] Traffic: {src_ip}:{src_port} -> {dst_ip}:{dst_port}'
         print(log_entry)
@@ -44,16 +60,17 @@ def log_packet(packet):
             conn_attempts_obj[src_ip][0] = conn_attempts_obj[src_ip][0]+1
             prev_attempts = deque(conn_attempts_obj[src_ip][1])
             prev_attempts.append(str(timestamp_val))
-            conn_attempts_obj[src_ip][1] = list(prev_attempts)
-            if len(prev_attempts) > CONN_ATTEMPT_THRESHOLD:
+            while len(prev_attempts) > CONN_ATTEMPT_THRESHOLD:
                 prev_attempts.popleft()
                 give_warning = True
+            conn_attempts_obj[src_ip][1] = list(prev_attempts)
+
             
         with open(CONN_ATTEMPTS_FILE, 'w') as file:
             json.dump(conn_attempts_obj, file)
 
         if give_warning:
-            logging.warning(f'ip {src_ip} has sent more than {CONN_ATTEMPT_THRESHOLD} connection requests')    
+            logging.warning(f'WARNING: ip {src_ip} has sent more than {CONN_ATTEMPT_THRESHOLD} connection requests')    
 
     except Exception as e:
         logging.error(f'Error processing packet: {e}')
