8e51a71c969a5f02bfe6b884ab3916b05f0cb7fc
Alex Burrows <burrows.alexj@gmail.com>
2025-01-02 02:46:40 +0000

    rewrite timestamps of conn_attempts_obj

--- traffic_monitor.py
+++ traffic_monitor.py
@@ -3,41 +3,46 @@ import logging
 from datetime import datetime
 import os
 import json
+from collections import deque
 
-
+CONN_ATTEMPT_THRESHOLD = 2
 LOG_FILE = 'traffic.log'
 CONN_ATTEMPTS_FILE = "connection_attempts.json"
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(message)s')
 
 def get_timestamps():
-    timestamp = datetime.now().strftime('%Y-%m-%d %H-:%M:%S')
-    timestamp_arr = str(datetime.now()).split(' ')[1].split(':')
-    secTime = float(timestamp_arr[2]) + int(timestamp_arr[1])*60 + int(timestamp_arr[0])*3600
+    timestamp_val = datetime.now()
+    timestamp_str = timestamp_val.strftime('%Y-%m-%d %H-:%M:%S')
 
-    return timestamp, secTime
+    return timestamp_str, timestamp_val
 
 
 def log_packet(packet):
     try:
-        timestamp, secTime = get_timestamps()
+        timestamp_str, timestamp_val = get_timestamps()
 
         src_ip = packet[0][1].src
         dst_ip = packet[0][1].dst
         src_port = packet[0][2].sport
         dst_port = packet[0][2].dport
 
-        log_entry = f'[{timestamp}] Traffic: {src_ip}:{src_port} -> {dst_ip}:{dst_port}'
+        log_entry = f'[{timestamp_str}] Traffic: {src_ip}:{src_port} -> {dst_ip}:{dst_port}'
         print(log_entry)
         logging.info(log_entry)
 
         conn_attempts_obj = {}
         if os.path.exists(CONN_ATTEMPTS_FILE):
+            # load the current conn_attempts_obj
             with open(CONN_ATTEMPTS_FILE, 'r') as file:
                 conn_attempts_obj = json.load(file)
-            print(f'conn_attemps_obj: {conn_attempts_obj}')
-            conn_attempts_obj[src_ip] = conn_attempts_obj.get(src_ip, 0) + 1
-            with open(CONN_ATTEMPTS_FILE, 'w') as file:
-                json.dump(conn_attempts_obj, file)
+
+        # up date the obj with the new log
+        if src_ip not in conn_attempts_obj:
+            conn_attempts_obj[src_ip] = [0, deque([timestamp_val])]
+        else:
+            conn_attempts_obj[src_ip][0] = conn_attempts_obj.get(src_ip)[0]+1
+        with open(CONN_ATTEMPTS_FILE, 'w') as file:
+            json.dump(conn_attempts_obj, file)
 
         if conn_attempts_obj[src_ip] > 2:
             logging.warning(f'ip {src_ip} has sent more than 2 connection requests')    
