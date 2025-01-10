f13f739cde5922fc221a71dde833107562ab5fc4
Alex Burrows <burrows.alexj@gmail.com>
2025-01-10 07:15:40 +0000

    Clear traffic.log after the database table

--- clear_logs.py
+++ clear_logs.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import sql_config
+import os
 
+LOG_FILE_PATH = 'traffic.log'
 db_config = {
     'host': sql_config.DB_HOST,
     'user': sql_config.DB_USER,
@@ -25,5 +27,14 @@ def clear_table():
             conn.close()
             print('MySQL connection closed.')
 
+def clear_log_file():
+    try:
+        with open(LOG_FILE_PATH, 'w') as file:
+            file.write("")
+        print(f'{LOG_FILE_PATH} has been wiped')
+    except OSError as e:
+        print(f'Error clearing log file: {e}')
+
 if __name__ == '__main__':
     clear_table()
+    clear_log_file()
